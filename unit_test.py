d51bf5f8d4500bc38b039d17eb744f0aae5a6c4b
akashsachithanandham <46894142+akashsachithanandham@users.noreply.github.com>
2021-02-04 13:29:37 +0530

    added coveralls

--- unit_test.py
+++ unit_test.py
@@ -2,6 +2,10 @@ import unittest
 import random
 
 from validator import verify
+from validator import validateCvv
+from validator import validateName
+from validator import validateExpiry
+from validator import checkCardValidity
 #from generate import generate
 
 """Unit tests class.
@@ -15,6 +19,33 @@ class UnitTest(unittest.TestCase):
         self.assertEqual(verify(785201), False)
         self.assertEqual(verify(424242424242), True)
 
+    def testValidateCvv(self):
+        print(validateCvv("314"))
+        assert validateCvv("314") == True
+        assert validateCvv("615") == True
+        assert validateCvv("-91") == False
+        assert validateCvv("1678797887") == False
 
+
+    def testValidateName(self):
+        assert validateName("Akash") == True
+        assert validateName("Balaji") == True
+        assert validateName("8965555") == False
+       
+
+
+    def testValidateExpiry(self):
+        assert validateExpiry("07/22") == True
+        assert validateExpiry("08/30") == True
+        assert validateExpiry("89/11") == False
+        
+    def testCheckCardValidity(self):
+        assert checkCardValidity(
+            "Akash", "79927398713", "343", "03/22") == True
+        assert checkCardValidity(
+            "Sanjay", "79927391134", "51", "04/23") == False
+        assert checkCardValidity(
+            "Buvan", "79927391134", "113", "03/27") == False
+        
 if __name__ == '__main__':
     unittest.main()
--- validator.py
+++ validator.py
@@ -1,5 +1,8 @@
 from cchashlib import stringToIntegerArray
 from cchashlib import luhnAddition
+import sys
+import re
+from datetime import date
 
 def verify(ccNumber):
     """This function adds up all the digits in a credit card number using Luhn's Addition on one digit out of two.
@@ -32,6 +35,79 @@ def verify(ccNumber):
     #We return the result
     res = res%10
     return res==0
-#print("Hello")
-#print(verify(102300000))
-print(verify(424242424242))
+
+def validateName(name):
+    exp = r'^[a-zA-Z ]+$'
+    if re.search(exp, name):
+        return True
+    else:
+        return False
+
+# helper Function for Number validity
+
+
+def sum_digits(digit):
+    if digit < 10:
+        return digit
+    else:
+        sum = (digit % 10) + (digit // 10)
+        return sum
+
+# check number validity
+# check cvv validity
+def validateCvv(cvv):
+    exp = r'\d{3}'
+    if re.search(exp, cvv):
+        return True
+    else:
+        return False
+
+# check card Date validatity
+
+
+def validateExpiry(expiryDate):
+    today = date.today()
+    todayString = str(today)
+    todayMM = todayString[5:7]
+    todayYY = todayString[2:4]
+
+    expiryMM = expiryDate[0:2]
+    expiryYY = expiryDate[3:]
+
+    expiryYYInt = int(expiryYY)
+    expiryMMInt = int(expiryMM)
+    todayMMInt = int(todayMM)
+    todayYYInt = int(todayYY)
+
+    # print("Today ints", todayMMInt, " ", todayYYInt)
+    # print("Expiry ints", expiryMMInt, " ", expiryYYInt)
+
+    if(expiryYYInt < todayYYInt):
+        return False
+    elif(expiryYY == todayYY):
+        if(expiryMMInt < todayMMInt):
+            return False
+        else:
+            return True
+    else:
+        return True
+
+# checking all details of a card
+
+
+def checkCardValidity(cardName, cardNumber, cardCvv, cardExpiry):
+    cardNumValid = verify(cardNumber)
+    cardNameValid = validateName(cardName)
+    cardCvvValid = validateCvv(cardCvv)
+    cardExpiryValid = validateExpiry(cardExpiry)
+    return(cardNameValid and cardNameValid and cardCvvValid and cardExpiryValid)
+
+
+if __name__ == "__main__":
+    cardName = "AkashS"
+    cardNumber = "79927398713"
+    cardCvv = "388"
+    cardExpiry = "07/21"
+
+    print("Card Validation ", checkCardValidity(
+        cardName, cardNumber, cardCvv, cardExpiry))
